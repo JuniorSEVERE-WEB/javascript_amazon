95b7f5308a619dc7a207680fd78ddac98bb2faa1
Junior SEVERE <severejunior2017@gmail.com>
2025-07-11 19:52:21 -0400

    ok

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,26 @@
 
 
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart)
+{
+  cart =[{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
-},
+    deliveryOptionsId:'1'
+    },
+    {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionsId: '2'
+    }];
+}
+ 
+
+function saveToStorage()
 {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId)
 {
@@ -24,9 +37,11 @@ export function addToCart(productId)
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: 1,
+          deliveryOptionsId: '1'
         });
       }
+      saveToStorage();
 }
 
 export function removeFromCart(productId)
@@ -39,4 +54,5 @@ export function removeFromCart(productId)
       }
     });
     cart = newCart;
+    saveToStorage();
 }
